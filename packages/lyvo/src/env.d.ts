196b629e146e8887e15b8d71e45ae206ab45ca64
d34dscene <d34dscene@mizuchi.dev>
2026-07-06 15:06:44 +0200

    fix(types): remove astro:content module type override

--- packages/lyvo/src/env.d.ts
+++ packages/lyvo/src/env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="astro/client" />
-
-declare module 'astro:content' {
-	export function getEntry(collection: string, id: string): Promise<any>;
-	export function getCollection(collection: string): Promise<any[]>;
-	export function defineCollection(config: any): any;
-}
